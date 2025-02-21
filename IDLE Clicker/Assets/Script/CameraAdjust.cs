b6f19efed06e136d852ec07900bc41e820ab28db
GUNZ <guncastation@gmail.com>
2025-02-21 14:48:44 +0700

    Version 1.8

---
+++ IDLE Clicker/Assets/Script/CameraAdjust.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjust : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float referenceWidth = 1080f; // ความกว้างของดีไซน์ดั้งเดิม
+    public float referenceHeight = 1920f; // ความสูงของดีไซน์ดั้งเดิม
+    public float baseOrthographicSize = 5f; // ค่า Size ที่ต้องการใช้เป็นค่าเริ่มต้น
+
+    void Start()
+    {
+        AdjustCameraSize();
+    }
+
+    void AdjustCameraSize()
+    {
+        float targetAspect = 9f / 16f; // อัตราส่วนจอที่ต้องการ (16:9)
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = screenAspect / targetAspect;
+
+        Camera cam = mainCamera != null ? mainCamera : Camera.main;
+
+        if (scaleHeight < 1.0f)
+        {
+            cam.orthographicSize = cam.orthographicSize / scaleHeight;
+        }
+    }
+}
---
+++ IDLE Clicker/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(); // ปิดเกม
+            Debug.Log("Game Exited"); // แสดงใน Console (ใช้ได้เฉพาะตอนทดสอบใน Editor)
+        }
+    {
+        if (Input.GetKeyDown(KeyCode.F)) // กด F เพื่อสลับโหมดจอ
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+    }
+    }
+}
